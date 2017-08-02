b98c567d5497d3b5d0d48492aa3319f793b5d65f
yling0 <xyun2016@gmail.com>
2017-08-02 11:28:37 -0400

    fist commit

---
+++ src/main/java/me/yling/demo/Demowithhtml0802Application.java
@@ -0,0 +1,12 @@
+package me.yling.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demowithhtml0802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demowithhtml0802Application.class, args);
+	}
+}
